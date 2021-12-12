2d7ec0a0afa7988e80a16ea7ebcb20f1105a4cc3
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2021-12-12 15:49:25 -0300

    create switch test

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,8 +3,11 @@ import { render } from "@testing-library/react-native";
 
 import { Teste } from "../../screens/Teste";
 
-test("check if show correctly input user name placeholder", () => {
-  const { debug } = render(<Teste />);
+describe("Profile", () => {
+  it("should be show ther correctly in input user name placeholder", () => {
+    const { getByPlaceholderText } = render(<Teste />);
+    const inputName = getByPlaceholderText("Nome");
 
-  debug();
+    expect(inputName).toBeTruthy();
+  });
 });
