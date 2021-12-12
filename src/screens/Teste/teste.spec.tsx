e126d974bea2e7033686a7ea519361fe686b0f82
Renan Melo <renanmelo@MacBook-Air-de-Renan.local>
2021-12-12 16:32:05 -0300

    moving the test to inside the page

--- src/screens/Teste/teste.spec.tsx
+++ src/screens/Teste/teste.spec.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
 
-import { Teste } from "../../screens/Teste";
+import { Teste } from ".";
 
-describe("Profile", () => {
+describe("Teste", () => {
   it("should be show ther correctly in input user name placeholder", () => {
     const { getByPlaceholderText } = render(<Teste />);
     const inputName = getByPlaceholderText("Nome");
